0b1d991d4cdd7801fc73c1e3030f1f682c065cfc
onras123 <42933297+onras123@users.noreply.github.com>
2018-09-17 16:13:58 +0530

    Add files via upload

---
+++ surilargest.py
@@ -0,0 +1,9 @@
+a=int(input("enter a number")):
+b=int(input("enter a number")):
+c=int(input("enter a number")):
+if(a>b & a>c):
+	print("a is the largest number"):
+elif(b>c):
+	print("b is the largest number"):
+else:
+    print("c is the largest number"):
